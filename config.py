3e2bd27649e53acca20402de219f171c65da158d
Miss Brutal <157978295+MissBrutal@users.noreply.github.com>
2025-07-25 22:22:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ APP_ID = int(os.environ.get("APP_ID", "21642624"))
 API_HASH = os.environ.get("API_HASH", "11af98e6c638725a6068667d3fc39c05")
 
 # Main
-OWNER_ID = int(os.environ.get("OWNER_ID", "1885113059"))
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 PORT = os.environ.get("PORT", "8080")
 
 # Database
